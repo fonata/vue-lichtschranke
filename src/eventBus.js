d4f5ae24eb08a34ecb1a8ff5876b8b239dc9c37b
Christian Bläul <christian.blaeul@startnext.com>
2020-11-15 01:27:50 +0100

    eventBus ist nun für das Senden und Empfangen von Nachrichten zuständig
    Zum Server und innerhalb der Vue-Anwendung.

--- src/eventBus.js
+++ src/eventBus.js
@@ -1,5 +1,31 @@
 // Diese Datei existiert, damit wir den Lichtschranken Events schicken können
 import Vue from 'vue'
 
-var eventBus = new Vue();
+const port = 5000;
+
+var data = {
+    socket: new WebSocket("ws://127.0.0.1:" + port)
+}
+
+let eventBus = new Vue({
+    data: function () {
+        return data;
+    },
+    created: function () {
+        console.log('created')
+        this.socket.onopen = function () {
+            this.send("get-inputs");
+        };
+        this.socket.onmessage = function (event) {
+            try {
+                let data = JSON.parse(event.data);
+                console.log('The server said: ' + JSON.stringify(data, null, 4));
+                eventBus.$emit(data.event, data.payload)
+            } catch (e) {
+                console.log('[ERROR] The server sent invalid JSON: ' + event.data);
+            }
+        }
+    }
+});
+
 export default eventBus;
